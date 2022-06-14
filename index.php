814c47d0fe8db64be4821f9c3f1f0ac273fdbf84
MaksimovicOff <maximsannikovwork@gmail.com>
2022-06-14 17:33:32 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Charis+SIL:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>ReactJS</title>
+</head>
+<body>
+<div class="base">
+	<?php
+	include_once 'pages/header.html';
+	include_once 'pages/content_out.html';
+	?>
+</div>
+</body>
+</html>
+
