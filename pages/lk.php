1e454f2d78d194836c41366f2d7fa807fec0b296
MaksimovicOff <maximsannikovwork@gmail.com>
2022-06-15 17:40:52 +0400

    Create lk.php

---
+++ pages/lk.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Charis+SIL:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	<title>ReactJS</title>
+</head>
+<body>
+<div class="base">
+	<?php
+	session_start();
+	include_once 'header.html';
+    require 'db.php';
+    $str_out = "SELECT * FROM `users` WHERE `id` = '".$_SESSION['user']['id']."';";
+    $run_out = mysqli_query($connect,$str_out);
+    $user = mysqli_fetch_array($run_out);
+	?>
+	<div class="content_lk">
+		<div class="header_lk">
+			<div class="header_lk_t">Ваше имя - <?php echo $user['name']?></div>
+     		<div class="header_lk_t">Ваш Email - <?php echo $user['email']?></div>
+			<div class="header_lk_t"><a href="exit.php">Выйти</a></div>
+		</div>
+		<form action="game.php" class="game">
+        	<button class="start">Start</button>
+      	</form>
+	</div>
+</div>
+<script>
+    const fromStorage = localStorage.getItem('result');
+    if (fromStorage) {
+      document.querySelector('.result').innerHTML = fromStorage;
+    }
+
+    const gou = localStorage.getItem('gou');
+      document.querySelector('.results').style.display = 'inline-block';
+      document.querySelector('.results').innerHTML = gou;
+
+  </script>
+</body>
+</html>
