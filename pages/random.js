2312b676449f06ef15fd7a326708d052a472df9d
MaksimovicOff <maximsannikovwork@gmail.com>
2022-06-16 16:13:49 +0400

    Update random.js

--- pages/random.js
+++ pages/random.js
@@ -10,3 +10,22 @@ function check() {
     document.getElementById("task").innerHTML = current[0];
     document.getElementById('answer').value = '';
 }
+$('button.save1').on('click', function(){
+  localStorage.setItem('result', document.querySelector('.check').innerHTML);
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const check = document.querySelector('.check').innerHTML;
+  const mant = document.querySelector('.mant')
+  div.classList.add('gou');
+  const get = localStorage.getItem('result');
+  div.innerHTML = get;
+  mant.appendChild(div);
+  localStorage.setItem('gou', document.querySelector('.mant').innerHTML);
+})
+function Saved(){
+  const gou = localStorage.getItem('gou');
+  if (gou) {
+    document.querySelector('.mant').innerHTML = gou;
+  }
+}
+Saved();
