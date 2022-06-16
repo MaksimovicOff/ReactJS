0640cfb12f2ae50101e307d56ecf9adafe3fdee8
MaksimovicOff <maximsannikovwork@gmail.com>
2022-06-16 16:13:47 +0400

    Update Log_I.php

--- pages/Log_I.php
+++ pages/Log_I.php
@@ -27,5 +27,21 @@
 </div>
 </body>
 <script src="../jquery-3.6.0.min.js"></script>
-<script src="log_in.js"></script>
+<script>
+  $('button').on('click', function(){
+    const email = $('input.email').val();
+    const password = $('input.password').val();
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+  })
+function Auto() {
+  const fromStorage = localStorage.getItem('email');
+  const fromStorage1 = localStorage.getItem('password');
+  if (fromStorage || fromStorage1) {
+    $('input.email').val(fromStorage);
+    $('input.password').val(fromStorage1);
+  }
+}
+Auto();
+</script>
 </html>
